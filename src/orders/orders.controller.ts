d37486ce7c73e0918f2e6fc31a44ba3b6efd3aa3
Nicolay <nicolaymorenoherrera@gmail.com>
2024-09-13 16:26:07 -0500

    tested connection between client-gateway and orders-ms

--- src/orders/orders.controller.ts
+++ src/orders/orders.controller.ts
@@ -1,22 +1,37 @@
-import { Controller, Get, Post, Body, Param } from '@nestjs/common';
+import { Controller, Get, Post, Body, Param, Inject, Patch } from '@nestjs/common';
 import { CreateOrderDto } from './dto/create-order.dto';
+import { ORDER_SERVICE } from 'src/config';
+import { ClientProxy } from '@nestjs/microservices';
 
 @Controller('orders')
 export class OrdersController {
-  constructor() {}
+  constructor(
+
+    @Inject(ORDER_SERVICE)
+    private readonly ordersClient: ClientProxy,
+
+  ) {}
 
   @Post()
   create(@Body() createOrderDto: CreateOrderDto) {
-    // return this.ordersService.create(createOrderDto);
+    console.log('pas por aquí')
+    return this.ordersClient.send( 'createOrder', createOrderDto );
   }
 
   @Get()
   findAll() {
-    // return this.ordersService.findAll();
+    return this.ordersClient.send( 'findAllOrders', {} );
   }
 
   @Get(':id')
-  findOne(@Param('id') id: string) {
-    // return this.ordersService.findOne(+id);
+  findOne( @Param('id') id: string ) {
+    return this.ordersClient.send( 'findOneOrder', id );
   }
+
+  @Patch(':id')
+  changeOrderStatus( @Param('id') id: string ) {
+    return this.ordersClient.send( 'changeOrderStatus', { id } )
+
+  }
+
 }
