4e3163df027030014e643fdda5658555f12da8c4
Nicolay <nicolaymorenoherrera@gmail.com>
2024-09-22 17:06:28 -0500

    feat: Add console log message in main.ts

--- src/main.ts
+++ src/main.ts
@@ -22,6 +22,9 @@ async function bootstrap() {
   app.useGlobalFilters( new RpcCustomExceptionFilter() );
 
   await app.listen( envs.port );
+
+  console.log('Hola mundo - Primer cambio!!');
+
   logger.log(`Gateway running on port ${ envs.port }`);
 
 }
