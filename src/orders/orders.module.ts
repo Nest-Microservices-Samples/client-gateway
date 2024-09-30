3a9e4a03eb9a8cdc76c1c5a811ead4b7330642ab
Nicolay <nicolaymorenoherrera@gmail.com>
2024-09-29 20:49:27 -0500

    +++

--- src/orders/orders.module.ts
+++ src/orders/orders.module.ts
@@ -2,6 +2,7 @@ import { Module } from '@nestjs/common';
 import { OrdersController } from './orders.controller';
 import { NatsModule } from 'src/transports/nats.module';
 
+
 @Module({
   imports: [NatsModule],
   controllers: [OrdersController],
